54b31b26bc8fbd6a5e85e0eb89b6b9ca822c3898
simonrono3 <simonrono3@gmail.com>
2026-08-27 00:31:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const SUPABASE_URL =  'https://xijoilglijvwozklatxa.supabase.co'// Weka Project URL yako
+const SUPABASE_URL = 'https://xijoilglijvwozklatxa.supabase.co'// Weka Project URL yako
 const SUPABASE_KEY = 'sb_publishable_qAINvRS-w-MlxngWsSHa9Q_QBbF1mkp' // Weka anon public key yako
